4e9c0fee4e746ea29b36f8e6d0ae39ff13827e31
Taylor Thurlow <thurlow@hey.com>
2024-02-02 21:42:05 -0800

    Alt+Backspace for DELETE

--- keyboards/mode/m256wh/keymaps/taylorthurlow/config.h
+++ keyboards/mode/m256wh/keymaps/taylorthurlow/config.h
@@ -1,2 +1,10 @@
 #define PERMISSIVE_HOLD
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_ALT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]) {
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
